a6eb74cd15aeb462ccb1e2957f488cf1ee1538f7
ashish <ashishjha14411@gmail.com>
2025-07-18 03:32:38 +0530

    added auth routes

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks,Q
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
 from app.schemas.auth import PasswordChangeRequest, SignUpRequest, SignUpResponse, LoginRequest, RefreshTokenRequest, TokenPair, MessageResponse, UserProfile, UserUpdate,VerifyOtpRequest
-from app.services.auth import  create_user
+from app.services.auth import  change_password, create_user, get_current_user,login_user, logout_user, update_profile,verify_email, verify_otp
 from app.dependencies import get_db, get_password_hasher, get_mailer
 from app.models.user import User
 from app.utils.rate_limiter import rate_limit
@@ -40,3 +40,48 @@ def signup(
         username=user.username,
         message="User created. Please check your email for verification."
     )
+
+@router.post("/login",response_model=TokenPair,status_code=status.HTTP_200_OK)
+def login(data:LoginRequest,db: Session=Depends(get_db), hasher = Depends(get_password_hasher)):
+    return login_user(db,data,hasher)
+
+@router.post("/logout",response_model=MessageResponse,status_code=status.HTTP_200_OK)
+def logout(data:RefreshTokenRequest, db:Session=Depends(get_db)):
+    return logout_user(db=db,refresh_token=data.refresh_token)
+
+@router.post("/refresh", response_model=TokenPair,status_code=status.HTTP_200_OK)
+def refresh_token(data: RefreshTokenRequest,db:Session=Depends(get_db)):
+    return refresh_access(db, data)
+
+@router.get("/verify-email",response_model=MessageResponse, status_code=status.HTTP_200_OK)
+def verfify_email_link(token: str = Query(..., description="Email-Verification JWT Token"), db:Session=Depends(get_db)):
+    return verify_email(token, db)
+
+@router.post("/verify-otp", response_model=MessageResponse,status_code=status.HTTP_200_OK)
+
+def otp_verify(data:VerifyOtpRequest,db:Session=Depends(get_db)):
+    return verify_otp(data,db)
+
+@router.get("/me", response_model=UserProfile, status_code=status.HTTP_200_OK)
+
+def read_users_me(current=Depends(get_current_user)):
+    return current
+
+@router.patch("/me", response_model=UserProfile, status_code=status.HTTP_200_OK) 
+
+def patch_users_me(data: UserUpdate, current=Depends(get_current_user), db:Session = Depends(get_db)):
+    return update_profile(current, data, db)
+
+@router.patch("/me/password",status_code=status.HTTP_200_OK)
+
+def patch_users_me_password(
+    data: PasswordChangeRequest,
+    current= Depends(get_current_user),
+    hasher=Depends(get_password_hasher),
+    db: Session= Depends(get_db)
+):
+    return change_password(current,data,hasher,db)
+
+
+
+
--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -14,7 +14,7 @@ from app.models.otp_verification import OTPVerification
 from app.utils.security import hash_password, create_access_token,verify_password,decode_access_token
 from app.core.config import settings
 from app.models.otp_verification import OTPVerification
-
+from app.models.token_blacklist import TokenBlacklist
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/auth/login")
 
 def create_user(
@@ -97,3 +97,115 @@ def create_user(
     )
 
     return new_user
+
+def login_user(db:Session, data:LoginRequest,hasher) -> TokenPair:
+    user = db.query(User).filter(User.username == data.username).first()
+    if not user or not verify_password(data.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid username or password"
+        )
+    access = create_access_token({"user_id": user.id})
+    refresh = create_access_token(
+        {"user_id": user.id},
+        expires_delta=timedelta(days=14) #14 days expiry for long retained token boletoh refresh token
+    )
+    return TokenPair(access_token=access,refresh_token=refresh)
+
+def logout_user(db:Session,refresh_token:str) -> None:
+    try:
+        payload = decode_access_token(refresh_token)
+        jti = payload.get("jti")
+        expires_at = datetime.fromtimestamp(payload.get("exp"))
+        
+        blacklist_entry = TokenBlacklist(jti=jti, expires_at=expires_at)
+        db.add(blacklist_entry)
+        db.commit()
+    except Exception:
+        pass
+    
+    return {"message":"You have been sucessfully logged out"}
+    
+
+def refresh_access(db:Session, data:RefreshTokenRequest) -> TokenPair:
+    try:
+        payload = decode_access_token(data.refresh_token)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Invalid refresh token"
+        )
+    user = db.query(User).get(payload.get("user_id"))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="User not found"
+        )
+    access = create_access_token(
+        {"user_id": user.id}, expires_delta=timedelta(hours=12)
+    )
+    
+def verify_email(token:str, db: Session) -> MessageResponse:
+    try:
+        payload = decode_access_token(token)
+    except Exception:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = " User not Found"
+        )
+    user = db.query(User).get(payload.get("user_id"))
+    if user.is_verified:
+        return MessageResponse(message = "Email is Verified")
+    user.is_verified = True
+    db.commit()
+    return MessageResponse("Email is verified Successfully")
+
+def verify_otp(data: VerifyOtpRequest, db: Session) -> MessageResponse:
+    #filter by email
+    user = db.query(User).filter_by(email=data.email).first()
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail="User not Found"
+        )
+        
+    otp = {
+        db.query(OTPVerification).filter_by(user_id=user.id, otp_code=data.otp_code, used = False).first()
+    }
+    if not otp or otp.expires_at < datetime.utcnow():
+        raise HTTPException(
+            status_code = status.HTTP_400_NOT_FOUND,
+            detail="Invalid or expired OTP"
+        )
+    otp.used =True
+    user.is_otp_verified = True
+    db.commit()
+    return MessageResponse(message="OTP verified successfully")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    try:
+        payload = decode_access_token(token)
+        user_id = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    except PyJWTError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,"Invalid or expired token")
+    
+    user = db.query(User).get(user_id)
+    if not user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "User not found")
+    return user
+
+def update_profile(current: User,data: UserUpdate,db: Session) -> User:
+    for field, value in data.dict(exclue_unset=True).items():
+        setattr(current, field,value)
+    db.commit()
+    db.refresh(current)
+    return current
+
+def change_password(current:User, data:PasswordChangeRequest, hasher, db:Session):
+    if not hasher.verify(data.old_password, current.password_hash):
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Incorrect old password")
+    current.password_hash = hasher.hash(data.new_password)
+    db.commit()
+    return{"message": "Password changed successfully"}
